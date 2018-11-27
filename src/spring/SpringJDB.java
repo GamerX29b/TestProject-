ef4f47b707eed18702617f3eaf44f839ae20b4e6
User <sergej-93@mail.ru>
2018-11-27 19:14:34 +0600

    Вечерний коммит + немного мозгов.

--- src/spring/SpringJDB.java
+++ src/spring/SpringJDB.java
@@ -4,16 +4,18 @@ import oracle.jdbc.pool.OracleDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Configuration
 @ConfigurationProperties("oracle")
-public class SpringJDB {
+@Component
+public class SpringJDB implements Test {
 
-    @Bean
-    static DataSource dataSource() throws SQLException {
+    //@Bean
+    public DataSource getDataSource() throws SQLException {
         OracleDataSource dataSource = new OracleDataSource();
         dataSource.setURL("jdbc:oracle:thin:@192.168.126.50:1521:fkszdb");
         dataSource.setUser("WIDGET_Z");
